037a5875689d52882d9ddcba92fba0b6c0e32824
Gluz <brunogluszczukjobs@gmail.com>
2023-03-12 09:19:18 -0300

    fix method names

--- 01_duck_behavior/mini_duck_simulator.py
+++ 01_duck_behavior/mini_duck_simulator.py
@@ -4,12 +4,12 @@ from fly_rocket_powered import FlyRocketPowered
 
 if __name__ == "__main__":
     mallard = MallardDuck()
-    mallard.performQuack()
-    mallard.performFly()
+    mallard.perform_quack()
+    mallard.perform_fly()
 
     # to change a duck's behavior at runtime
     # just call the duck's setter method for that behavior
     model = ModelDuck()
-    model.performFly()
-    model.setFlyBehavior(FlyRocketPowered())
-    model.performFly()
+    model.perform_fly()
+    model.set_fly_behavior(FlyRocketPowered())
+    model.perform_fly()
